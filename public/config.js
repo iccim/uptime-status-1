4384a59d96b922b3fb929c132d2f74b9b66b8a6a
iccim <92652512+iccim@users.noreply.github.com>
2021-10-17 06:29:45 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -6,15 +6,9 @@ window.Config = {
 
   // 站点链接
   SiteUrl: '/',
+  ApiDomain: 'zxjk.izxy.workers.dev',
 
-  // UptimeRobot Api 域名
-  // 只需填写域名部分，默认为官网域名
-  // 因官网 API 时不时的会 CROS 报错，可填自定义反代域名
-  // 详见 https://github.com/yb/uptime-status/ 说明
-  ApiDomain: 'api.uptimerobot.com',
-
-  // UptimeRobot Api Keys
-  // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
+  // Api Keys
   ApiKeys: [
     'ur1483427-92866f4c7354585d637bc0aa',
   ],
@@ -23,8 +17,6 @@ window.Config = {
   ShowLink: true,
 
   // 日志天数
-  // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
-  // 不过时间不要设置太长，容易卡，接口请求也容易失败
   CountDays: 60,
 
   // 导航栏菜单
